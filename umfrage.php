323e6ba0a2464027d7c5160d523f82a05a95b51d
Theragor <s72851@informatik.htw-dresden.de>
2016-05-04 14:50:58 +0200

    Update umfrage.php

--- umfrage.php
+++ umfrage.php
@@ -46,64 +46,53 @@
             <label><input name="knowntutor" type="radio" class="regular-radio big-radio" value="ja" required /> yes</label><!-- Pflichtauswahl -->
             <label><input name="knowntutor" type="radio" class="regular-radio big-radio" value="nein" required /> no </label>
 
-                    <h4>How good was/is the support of your Tutor?</h4>
-                    <!-- Wie gut war die Unterstützung deines Tutors -->
-                    <label>
-                        <input name="comtutor" type="radio" class="regular-radio big-radio" value="1" /> very good </label>
-                    <label>
-                        <input name="comtutor" type="radio" class="regular-radio big-radio" value="2" /> good </label>
-                    <label>
-                        <input name="comtutor" type="radio" class="regular-radio big-radio" value="3" /> ok </label>
-                    <label>
-                        <input name="comtutor" type="radio" class="regular-radio big-radio" value="4" /> bad </label>
-                    <label>
-                        <input name="comtutor" type="radio" class="regular-radio big-radio" value="5" /> very bad </label>
+            <h4>How good was/is the support of your Tutor?</h4>
+            <!-- Wie gut war die Unterstützung deines Tutors -->
+            <label><input name="comtutor" type="radio" class="regular-radio big-radio" value="1" /> very good </label>
+            <label><input name="comtutor" type="radio" class="regular-radio big-radio" value="2" /> good </label>
+            <label><input name="comtutor" type="radio" class="regular-radio big-radio" value="3" /> ok </label>
+            <label><input name="comtutor" type="radio" class="regular-radio big-radio" value="4" /> bad </label>
+            <label><input name="comtutor" type="radio" class="regular-radio big-radio" value="5" /> very bad </label>
 
-                    <h4>How good was/is the language of your Tutor?</h4>
-                    <!-- Wie gut waren die Komunikationsskills deinens Tutors -->
-                    <label>
-                        <input name="speachtutor" type="radio" class="regular-radio big-radio" value="1" /> very good </label>
-                    <label>
-                        <input name="speachtutor" type="radio" class="regular-radio big-radio" value="2" /> good </label>
-                    <label>
-                        <input name="speachtutor" type="radio" class="regular-radio big-radio" value="3" /> ok </label>
-                    <label>
-                        <input name="speachtutor" type="radio" class="regular-radio big-radio" value="4" /> bad </label>
-                    <label>
-                        <input name="speachtutor" type="radio" class="regular-radio big-radio" value="5" /> very bad </label>
-                </p>
+            <h4>How good was/is the language of your Tutor?</h4>
+            <!-- Wie gut waren die Komunikationsskills deinens Tutors -->
+            <label><input name="speachtutor" type="radio" class="regular-radio big-radio" value="1" /> very good </label>
+            <label><input name="speachtutor" type="radio" class="regular-radio big-radio" value="2" /> good </label>
+            <label><input name="speachtutor" type="radio" class="regular-radio big-radio" value="3" /> ok </label>
+            <label><input name="speachtutor" type="radio" class="regular-radio big-radio" value="4" /> bad </label>
+            <label><input name="speachtutor" type="radio" class="regular-radio big-radio" value="5" /> very bad </label>
+        </p>
 
-                <p>
-                    <!-- Zweiter Abschnitt: Die Erst-Semester-Woche -->
-                    <hr />
-                    <h3>Pt2: The First-Semester-Week</h3>
-                </p>
+        <p><!-- Zweiter Abschnitt: Die Erst-Semester-Woche -->
+            <hr />
+            <h3>Pt2: The First-Semester-Week</h3>
+        </p>
 
-                <p>
-                    <!-- Dritter Abschnitt: Allgemein -->
-                    <hr />
-                    <h3>Pt3: Generel</h3> 150,2-16 81%
-                    <h4>How good was/is the Tutorprogramm.</h4>
-                    <!-- Wie fandest Du das Tutorenprogramm im gesamten? -->
-<label><input name="programm" type="radio" class="regular-radio big-radio" value="1" /> very good </label>
-<label><input name="programm" type="radio" class="regular-radio big-radio" value="2" /> good </label>
-<label><input name="programm" type="radio" class="regular-radio big-radio" value="3" /> ok </label>
-<label><input name="programm" type="radio" class="regular-radio big-radio" value="4" /> bad </label>
-<label><input name="programm" type="radio" class="regular-radio big-radio" value="5" /> very bad </label>
+        <p><!-- Dritter Abschnitt: Allgemein -->
+            <hr />
+            <h3>Pt3: Generel</h3>
+            
+            <h4>How good was/is the Tutorprogramm.</h4>
+            <!-- Wie fandest Du das Tutorenprogramm im gesamten? -->
+            <label><input name="programm" type="radio" class="regular-radio big-radio" value="1" /> very good </label>
+            <label><input name="programm" type="radio" class="regular-radio big-radio" value="2" /> good </label>
+            <label><input name="programm" type="radio" class="regular-radio big-radio" value="3" /> ok </label>
+            <label><input name="programm" type="radio" class="regular-radio big-radio" value="4" /> bad </label>
+            <label><input name="programm" type="radio" class="regular-radio big-radio" value="5" /> very bad </label>
 
-                    <h4>What was missing or what should we improve?</h4>
-                    <textarea name="bad_missing" type="text" placeholder="optional"></textarea>
+            <h4>What was missing or what should we improve?</h4>
+            <textarea name="bad_missing" type="text" placeholder="optional"></textarea>
 
-                    <h4>What did you really liked?</h4>
-                    <textarea name="good" type="text" placeholder="optional"></textarea>
-                </p>
+            <h4>What did you really liked?</h4>
+            <textarea name="good" type="text" placeholder="optional"></textarea>
+        </p>
 
-                <input type="submit" value="Submit">
-            </form>
-        </section>
-    </div>
+        <input type="submit" value="Submit">
+    </form>
+</section>
+</div>
 
-    <?php include 'post.php'; ?>
+<?php include 'post.php'; ?>
 
 </body>
 
